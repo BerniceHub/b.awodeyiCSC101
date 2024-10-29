f88e4f512164845ae1d674320592a0aa7a9d5305
unknown <bernice.awodeyi@pau.edu.ng>
2024-10-29 08:37:59 +0100

    (My third Rust project commit)

---
+++ week-2/project_3.rs
@@ -0,0 +1,9 @@
+fn main() {
+	let p = 510000; //original price
+	let r = 5; // rate of depreciation
+	let n = 3; //time
+
+    //value of TV after 3 years 
+	let a = p * ( 1 - (r / 100)) ^ n; //formula for amount
+	println!("The amount is {}", a);
+}

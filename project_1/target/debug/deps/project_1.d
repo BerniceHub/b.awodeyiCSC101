03f3dc7de863b18417f2c38d1951e9448446831b
BerniceHub <bernice.awodeyi@pau.edu.ng>
2024-11-24 21:21:17 +0100

    Delete project_1 directory

--- project_1/target/debug/deps/project_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\HP\b.awodeyiCSC101\week-5\project_1\target\debug\deps\project_1.exe: src/main.rs
-
-C:\Users\HP\b.awodeyiCSC101\week-5\project_1\target\debug\deps\project_1.d: src/main.rs
-
-src/main.rs:
